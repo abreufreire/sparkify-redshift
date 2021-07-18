c91f76cc0fda3f40c2621eef877490be8b9a741b
abreufreire <abreufreire@outlook.com>
2021-07-18 01:07:28 +0100

    add funcs to load stag & prod tables

--- etl.py
+++ etl.py
@@ -1,32 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    loads/copies song & log data from S3 into staging tables.
+    :param cur: postgres cursor
+    :param conn: postgres connection
+    :return: none
+    """
+
     for query in copy_table_queries:
+        print("\nexecuting: {}".format(query))
         cur.execute(query)
         conn.commit()
+    print("\ndata loaded into staging tables.")
 
 
 def insert_tables(cur, conn):
+    """
+    inserts data from staging tables into analytics tables (star-schema).
+    :param cur: postgres cursor
+    :param conn: postgres connection
+    :return: none
+    """
     for query in insert_table_queries:
+        print("\nexecuting: {}".format(query))
         cur.execute(query)
         conn.commit()
+    print("\ndata inserted into analytics tables.")
 
 
-def main():
+def etl():
+    # gets parameters from config file dwh.cfg
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read_file(open("dwh.cfg"))
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    # connection to redshift database
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(
+        config.get("CLUSTER", "DWH_HOST"),
+        config.get("CLUSTER", "DWH_DB_NAME"),
+        config.get("CLUSTER", "DWH_DB_USER"),
+        config.get("CLUSTER", "DWH_DB_PASSWORD"),
+        config.get("CLUSTER", "DWH_PORT")
+    ))
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
+
     insert_tables(cur, conn)
 
     conn.close()
 
 
 if __name__ == "__main__":
-    main()
+    etl()
